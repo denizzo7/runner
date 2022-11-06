e7e3b0ac37d5c5ac90847025b6a1a90d6a6b149f
Deniz <denizyildirimw7@gmail.com>
2022-11-06 19:29:44 +0300

    commited

---
+++ runner/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacle : MonoBehaviour
+
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle");
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
